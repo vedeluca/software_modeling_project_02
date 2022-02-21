b1130111c93d1ebc8c0fef3bdb9247031c5871b9
vedeluca <vedeluca@comcast.net>
2022-02-20 20:54:50 -0500

    quick fix to abstract class

--- JSON_Processing_Library/Objects/DataObject.cs
+++ JSON_Processing_Library/Objects/DataObject.cs
@@ -14,10 +14,6 @@ namespace JsonProcessing.Objects
         {
             node = new DataNode();
         }
-        public DataObject(DataNode parent) : this()
-        {
-            node = new DataNode(parent);
-        }
         public abstract void Add(string key, dynamic? value, int line);
         new public abstract string ToString();
         public abstract string ToString(string tabs);
